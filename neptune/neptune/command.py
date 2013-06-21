4f40bafc74015896b8dc42d18a4b64101f4704e9
Juti Noppornpitak <jnopporn@shiroyuki.com>
2013-06-21 01:30:07 -0400

    Added the command to delete the detabase.

---
+++ neptune/neptune/command.py
@@ -0,0 +1,19 @@
+class Database(object):
+    """ Command to create a new user
+    """
+    def __init__(self, db):
+        self._db = db
+    
+    def init(self):
+        self._session = self._db.open_session()
+
+    def clean_up(self):
+        del self._session
+    
+    def define_arguments(self, argument_parser):
+        argument_parser.add_argument('-d', '--delete', action='store_true')
+
+    def execute(self, args):
+        if args.delete:
+            self._db.db.connection.drop_database(self._db._name)
+            print('The database "{}" has been deleted.'.format(self._db._name))
--- neptune/neptune/security/model.py
+++ neptune/neptune/security/model.py
@@ -2,8 +2,9 @@ from tori.db.entity import entity
 
 @entity
 class Credential(object):
-    def __init__(self, login, password, salt, name):
+    def __init__(self, login, password, salt, name, roles=[]):
         self.login = login
         self.password = password
         self.salt = salt
         self.name = name
+        self.roles = roles
